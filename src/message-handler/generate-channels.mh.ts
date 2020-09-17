7fbf9730760bb05bef622f492b9b5f7c618c08a3
Lucas Simon <luke5227@gmail.com>
2020-09-16 22:48:41 -0700

    Allow bot to manage messages (so it can remove reactions)

--- src/message-handler/generate-channels.mh.ts
+++ src/message-handler/generate-channels.mh.ts
@@ -83,7 +83,7 @@ async function generateChannels(
     permissionOverwrites: [
       {
         id: client.user!,
-        allow: ['SEND_MESSAGES'],
+        allow: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
       },
       {
         id: guild.roles.everyone,
